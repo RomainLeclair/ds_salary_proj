5cdf10de9b2bade8d8531462f6250b2738b06386
RomainLeclair <rom1.leclair@gmail.com>
2020-05-28 12:05:55 +0200

    uploaded scraper and run code

---
+++ main.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 28 10:47:04 2020
+
+@author: Leclair Romain
+"""
+
+import glassdorScrapping as gs
+import pandas as pd
+
+path = r'C:\Users\Leclair Romain\Desktop\ds_salary_project\chromedriver.exe'
+df = gs.get_jobs('data scientist', 8, False, path, 15)
