8e4debae3ca8944c60b87ef2f9f90dc6f0df5eb8
RomainLeclair <rom1.leclair@gmail.com>
2020-05-29 14:26:26 +0200

    built models for pred glassdor

---
+++ data_collection.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 28 10:47:04 2020
+
+@author: Leclair Romain
+"""
+
+import glassdorScrapping as gs
+import pandas as pd
+
+path = r'C:\Users\Leclair Romain\Desktop\ds_salary_project\chromedriver'
+df = gs.get_jobs('data scientist', 100, False, path, 15)
+
+df.to_csv('glassdor_jobs.csv',index=False)
